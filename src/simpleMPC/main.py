a3e9e17638e49bc4889c50a828cedf7d2aafc43c
Elijah Chan <chanwaichong0352@gmail.com>
2025-10-25 08:37:04 -0400

    added test code

--- src/simpleMPC/main.py
+++ src/simpleMPC/main.py
@@ -1,4 +1,24 @@
 import numpy as np
 from robot_classes import RigidBodyState
+from robot_classes import RigidBodyTraj
+from trajectoryPlanner import generateConstantTraj
+from centroidalDynamics import continuousDynamics
+from centroidalDynamics import discreteDynamics
+import matplotlib.pyplot as plt
 
 state = RigidBodyState()
+traj = RigidBodyTraj()
+
+traj = generateConstantTraj(state, 0, 0.5, 0, 0, 0.1, 5, 0.6, 0.7)
+
+# plt.figure(figsize=(8, 6))
+# plt.plot(traj.x_pos_ref, traj.y_pos_ref)
+# plt.plot(traj.fl_foot_placement[0,:], traj.fl_foot_placement[1,:], 'o')
+# plt.plot(traj.fr_foot_placement[0,:], traj.fr_foot_placement[1,:], 'o')
+# plt.plot(traj.rl_foot_placement[0,:], traj.rl_foot_placement[1,:], 'o')
+# plt.plot(traj.rr_foot_placement[0,:], traj.rr_foot_placement[1,:], 'o')
+# plt.show()
+
+[A_continuous, B_continuous] = continuousDynamics(state, traj)
+
+[Ad, Bd] = discreteDynamics(A_continuous, B_continuous, 0.02)
