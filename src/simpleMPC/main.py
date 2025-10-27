94518d63eb53cbc034fbc046e9421e7fa86a54df
Elijah Chan <chanwaichong0352@gmail.com>
2025-10-27 10:55:32 -0700

    added test code

--- src/simpleMPC/main.py
+++ src/simpleMPC/main.py
@@ -9,15 +9,15 @@ import matplotlib.pyplot as plt
 state = RigidBodyState()
 traj = RigidBodyTraj()
 
-traj = generateConstantTraj(state, 0, 0.5, 0, 0, 0.1, 5, 0.6, 0.7)
+traj = generateConstantTraj(state, 0.5, 0, 0, 0, 0.1, 5, 1, 0.5)
 
-# plt.figure(figsize=(8, 6))
-# plt.plot(traj.x_pos_ref, traj.y_pos_ref)
-# plt.plot(traj.fl_foot_placement[0,:], traj.fl_foot_placement[1,:], 'o')
-# plt.plot(traj.fr_foot_placement[0,:], traj.fr_foot_placement[1,:], 'o')
-# plt.plot(traj.rl_foot_placement[0,:], traj.rl_foot_placement[1,:], 'o')
-# plt.plot(traj.rr_foot_placement[0,:], traj.rr_foot_placement[1,:], 'o')
-# plt.show()
+plt.figure(figsize=(8, 6))
+plt.plot(traj.x_pos_ref, traj.y_pos_ref)
+plt.plot(traj.fl_foot_placement[0,:], traj.fl_foot_placement[1,:], 'o')
+plt.plot(traj.fr_foot_placement[0,:], traj.fr_foot_placement[1,:], 'o')
+plt.plot(traj.rl_foot_placement[0,:], traj.rl_foot_placement[1,:], 'o')
+plt.plot(traj.rr_foot_placement[0,:], traj.rr_foot_placement[1,:], 'o')
+plt.show()
 
 [A_continuous, B_continuous] = continuousDynamics(state, traj)
 
